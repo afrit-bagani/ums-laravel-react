df71baf22ac8094504a9e621fb97c03f05f2c181
Office Laptop <o6228466@gmail.com>
2026-07-14 12:14:28 +0530

    fix: add flash applicant_code

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -45,6 +45,7 @@ class HandleInertiaRequests extends Middleware
             'flash' => [
                 'success' => fn () => $request->session()->get('success'),
                 'error' => fn () => $request->session()->get('error'),
+                'applicant_code' => fn () => $request->session()->get('applicant_code'),
             ],
         ];
     }
